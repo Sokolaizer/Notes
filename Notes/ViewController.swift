6d8b1d77604fc999567248bc15ce56ca151ef43e
Roman Kozlov <romanko-c@rumosmarmac.local>
2019-06-20 16:37:19 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Roman Kozlov on 20.06.2019.
+//  Copyright © 2019 Roman Kozlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

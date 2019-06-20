9a0e7336677f3db510e693193b9518cc20304e25
Roman Kozlov <romanko-c@rumosmarmac.local>
2019-06-20 18:01:14 +0300

    Add files

---
+++ Notes/Note.swift
@@ -0,0 +1,25 @@
+
+import UIKit
+
+struct Note {
+    let uid: String
+    let title: String
+    let content: String
+    let color: UIColor
+    let importance: Importance
+    let selfDestructDate: Date?
+    
+    init(uid: String = UUID().uuidString, title: String, content: String, color: UIColor = .white, importance: Importance, selfDestructDate: Date?) {
+        self.uid = uid
+        self.title = title
+        self.content = content
+        self.color = color
+        self.importance = importance
+        self.selfDestructDate = selfDestructDate
+    }
+}
+enum Importance: String {
+    case unimportant
+    case regular
+    case important
+}
---
+++ Notes/NoteExtensions.swift
@@ -0,0 +1,16 @@
+
+extension Note {
+    static func parse(json: [String: Any]) -> Note? {
+        print("Parsing")
+        return Note(title: "qe", content: "sfe", importance: .important, selfDestructDate: Date())
+    }
+    var json: [String: Any] {
+        switch (uid, title, content, color, importance, selfDestructDate) {
+        case let (_, _, _, _, importance, _):
+            print(importance)
+            return ["importance": "regular"]
+        default:
+            return ["importance": "regular"]
+        }
+    }
+}
